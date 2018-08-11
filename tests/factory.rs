d50aae8296a3f916ce4d9da12fea202d0bdba4c8
Gabriel Majeri <gabriel.majeri6@gmail.com>
2018-08-11 20:45:54 +0300

    Add the test code

---
+++ tests/factory.rs
@@ -0,0 +1,49 @@
+// Only makes sense to use COM on Windows.
+#![cfg(windows)]
+// Need this for the IUnknown implementation.
+#![feature(integer_atomics)]
+
+// Interface imports.
+use winapi::shared::dxgi::{IDXGIFactory, IDXGIFactoryVtbl, IDXGIObject, IDXGIObjectVtbl};
+use winapi::um::unknwnbase::{IUnknown, IUnknownVtbl};
+
+use com_impl::{com_implementation, com_interface};
+
+#[com_interface(IUnknown, IDXGIObject, IDXGIFactory)]
+pub struct FakeFactory {
+    variable: u64,
+}
+
+impl FakeFactory {
+    pub fn new() -> *mut IDXGIFactory {
+        let fact = Self {
+            __vtable: Self::create_vtable(),
+            __refs: Self::create_refs(),
+            variable: 12345,
+        };
+
+        let ptr = Box::into_raw(Box::new(fact));
+
+        ptr as *mut _
+    }
+}
+
+#[com_implementation(IUnknown, IDXGIObject)]
+impl FakeFactory {
+    fn set_private_data() {}
+    fn set_private_data_interface() {}
+    fn get_private_data() {}
+    fn get_parent() {}
+}
+
+#[com_implementation(IDXGIObject, IDXGIFactory)]
+impl FakeFactory {
+    fn create_software_adapter() {}
+
+    fn enum_adapters() {}
+
+    fn make_window_association() {}
+    fn get_window_association() {}
+
+    fn create_swap_chain() {}
+}
